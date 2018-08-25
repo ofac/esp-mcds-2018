7f996279aac5e611e1a947a97be9b99c0b7148a3
ofac <ofaczero@gmail.com>
2018-08-24 20:32:43 -0500

    Estructurado

---
+++ estructurado/ciclos.php
@@ -0,0 +1,12 @@
+<?php
+
+$cont = 0;
+
+while ($cont <= 100) {
+    if ($cont % 2 == 0) {
+        echo $cont."|";
+    }
+    $cont++;
+}
+
+?>
---
+++ estructurado/condicionales.php
@@ -0,0 +1,17 @@
+<?php
+
+$edad   = 26;
+$genero = "M";
+
+
+if($genero == "F") {
+    echo "No va a la Guerra!";
+} else {
+    if($edad >= 18) {
+        echo "Si va a la Guerra!";
+    } else {
+        echo "No va a la Guerra!";
+    }
+} 
+
+?>
